1a8d6c758b7981582d055645ee783244baf4f540
IavDar <elcandess@gmail.com>
2024-02-04 13:57:59 +0100

    12345

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -13,7 +13,7 @@ public class CurrencyConverter {
         exRate [2] = 0.03;
 
 
-        // для коммита4щдsd555sdf
+        // для коммита4щдsd555sdf12
         String[] availableCurr = {"EURO","USD", "TL"};
 
         PrintIntro(availableCurr);
