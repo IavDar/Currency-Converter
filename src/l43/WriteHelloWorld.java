1cf0a42fb5bce6ea12b75abc7e0cf303ef6f49e0
IavDar <elcandess@gmail.com>
2024-03-17 20:32:04 +0100

    Home work № 43

--- src/l43/WriteHelloWorld.java
+++ src/l43/WriteHelloWorld.java
@@ -1,10 +1,9 @@
 package l43;
 
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WriteHelloWorld {
 
@@ -17,17 +16,50 @@ public class WriteHelloWorld {
             System.out.print("Ошибка ввода вывода");
         }
 
-        try (
-                FileInputStream fis = new FileInputStream("resources\\file.txt")
-        ) {
+        try (FileInputStream fis = new FileInputStream("resources\\file.txt")) {
+
             Scanner scanner = new Scanner(fis);
             while(scanner.hasNext()) {
-                // читаем построчно пока есть данные
+
                 String line = scanner.nextLine();
                 System.out.print(line);
             }
         } catch (IOException e) {
             System.out.print("Ошибка ввода вывода");
         }
+
+        try (FileOutputStream fos = new FileOutputStream("resources\\file1.txt")) {
+
+            String greetings = "Hello World, I am Daria";
+
+            fos.write(greetings.getBytes());
+
+        } catch (IOException e) {
+            System.out.print("Ошибка ввода вывода");
+        }
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader("resources\\file1.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            System.out.print("Ошибка ввода вывода");
+        }
+
+
+        try (FileReader fr = new FileReader("resources\\file1.txt")) {
+            int i;
+
+            while((i =fr.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            System.out.print("Ошибка ввода вывода");
+        }
+
     }
+
 }
